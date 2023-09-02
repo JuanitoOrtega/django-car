8d6db5d9607ad4a44306a8a87bab5a7a6c086f04
Juanito Ortega <ortegaj83@gmail.com>
2023-09-02 11:09:03 -0400

    templates ready

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,7 +1,13 @@
-from .views import home
 from django.urls import path
+from .views import home, cars, about, services, contact, login, signup
 
 
 urlpatterns = [
     path('', home, name='home'),
+    path('cars/', cars, name='cars'),
+    path('about/', about, name='about'),
+    path('services/', services, name='services'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('signup/', signup, name='signup'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,51 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        'title': 'Welcome',
+    }
+    return render(request, 'pages/home.html', context)
+
+
+def cars(request):
+    context = {
+        'title': 'Cars',
+    }
+    return render(request, 'pages/cars.html', context)
+
+
+def about(request):
+    context = {
+        'title': 'About',
+    }
+    return render(request, 'pages/about.html', context)
+
+
+def services(request):
+    context = {
+        'title': 'Services',
+    }
+    return render(request, 'pages/services.html', context)
+
+
+def contact(request):
+    context = {
+        'title': 'Contact',
+    }
+    return render(request, 'pages/contact.html', context)
+
+
+def login(request):
+    context = {
+        'title': 'Login',
+    }
+    return render(request, 'pages/login.html', context)
+
+
+def signup(request):
+    context = {
+        'title': 'Sign Up',
+    }
+    return render(request, 'pages/signup.html', context)
