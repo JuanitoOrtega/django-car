be0475c1874f8678b2230a84620c684c1fbb56b9
Juanito Ortega <ortegaj83@gmail.com>
2023-09-07 01:51:56 -0400

    login and register

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import login, signup, logout, dashboard
+from .views import login, signup, logout_view, dashboard
 
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('signup/', signup, name='signup'),
-    path('logout/', logout, name='logout'),
+    path('logout/', logout_view, name='logout'),
     path('dashboard/', dashboard, name='dashboard'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import redirect, render
-from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -7,6 +9,19 @@ def login(request):
     context = {
         'title': 'Iniciar sesión',
     }
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'Inicio de sesión exitoso!')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Credenciales de acceso incorrectos!')
+            return redirect('login')
     return render(request, 'users/login.html', context)
 
 
@@ -14,12 +29,26 @@ def signup(request):
     context = {
         'title': 'Registrarse',
     }
-
     if request.method == 'POST':
-        messages.error(request, 'Este es un mensaje de error')
-        return redirect('signup')
-    else:
-        return render(request, 'users/signup.html', context)
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'El nombre de usuario ya existe!')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'El correo electrónico ya existe!')
+            else:
+                User.objects.create_user(first_name=firstname, last_name=lastname, username=username, email=email, password=password)
+                messages.success(request, 'Cuenta registrada exitosamente!')
+                return redirect('login')
+        else:
+            messages.error(request, 'Las contraseñas ingresadas no coinciden!')
+    return render(request, 'users/signup.html', context)
 
 
 def dashboard(request):
@@ -29,5 +58,6 @@ def dashboard(request):
     return render(request, 'users/dashboard.html', context)
 
 
-def logout(request):
+def logout_view(request):
+    logout(request)
     return redirect('home')
